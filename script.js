2305b39ad7f0feb5db9d392b35ee016c9ca96266
wojteckizakol3cki <168261526+wojteckizakol3cki@users.noreply.github.com>
2024-04-27 09:22:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const profilePicture = document.getElementById('profile-picture');
+
+document.addEventListener('mousemove', (e) => {
+    const { clientX, clientY } = e;
+    const { left, top, width, height } = profilePicture.getBoundingClientRect();
+    const centerX = left + width / 2;
+    const centerY = top + height / 2;
+    const angle = Math.atan2(clientY - centerY, clientX - centerX);
+    const rotation = angle * (180 / Math.PI) + 90;
+    profilePicture.style.transform = `rotate(${rotation}deg)`;
+});
